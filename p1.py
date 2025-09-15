da413fdc96afa5045f1aae036a923c605eafacf0
Mona Lisa <flame728xd@gmail.com>
2025-09-15 10:54:07 -0400

    Created python file

---
+++ p1.py
@@ -0,0 +1,8 @@
+# First program for my cs class
+# Created on 9/15/2025
+# Purpose: to learn
+# Editor: Jason
+
+name = input("What is your name? \n")
+age = input("How old are you? \n")
+print("hello " + name + " you are " + age + " years old")
